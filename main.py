4ad4c829c298b7087a02f728fb660cb5aec2b92b
sezer-muhammed <e230531@metu.edu.tr>
2022-05-29 14:58:43 +0300

    opencv example added

--- main.py
+++ main.py
@@ -1,15 +1,22 @@
-from matplotlib.pyplot import plot
 from reader import FingerForceSensorReader
 from plotter import DataVisualiser
-import time
 import numpy as np
-
+import cv2
 
 sensor = FingerForceSensorReader("COM3")
-plotter = DataVisualiser()
+#plotter = DataVisualiser()
 
+total_x = 450
+total_y = 450
 
-line = []
+canvas = np.ones((900,900,3), np.uint8) * 255
+
 while True:
-  output = sensor.get_all_data()
-  plotter.render(output)
+  output = sensor.get_most_recent_data()
+  total_x += int(-output[0])
+  total_y += int(output[1])
+  background = canvas.copy()
+  print(total_x, total_y)
+  cv2.circle(background, (total_x, total_y), 10, (0,0,255), -1, cv2.LINE_AA)
+  cv2.imshow("frame", background)
+  cv2.waitKey(1)
