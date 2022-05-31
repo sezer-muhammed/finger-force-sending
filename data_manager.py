3f0ae3f5484dc257e261fb8c7fb90bd801827f48
sezer-muhammed <e230531@metu.edu.tr>
2022-05-31 23:36:25 +0300

    data manager added

--- data_manager.py
+++ data_manager.py
@@ -8,10 +8,20 @@ class DataManager():
     self.data = np.empty((0, 3))
 
   def save(self, data = None):
-    if data != None:
-      self.data = data
-    np.save(self.save_file_name, self.data)
+    np.save(self.save_file_name, data)
 
   def load(self):
     self.data = np.load(self.load_file_name, allow_pickle = True)
     return self.data
+
+  def set_time(self):
+    self.start_time = time.time()
+
+  def return_real_time(self):
+    self.last_index = 0
+    index_time = time.time() - self.start_time
+    try:
+      temp_array = self.data[self.data[:, 2] < index_time]
+      return temp_array[-1]
+    except:
+      return np.array([[0, 0, 0]])
--- tester.py
+++ tester.py
@@ -6,22 +6,23 @@ import cv2
 
 #sensor = FingerForceSensorReader("COM3")
 #plotter = DataVisualiser()
-data_manager = DataManager("tracker")
-data_manager.save()
+data_manager = DataManager("empy", "empty.npy")
+data_manager.load()
+data_manager.set_time()
 
 
-exit()
 total_x = 450
 total_y = 450
 
 canvas = np.ones((900,900,3), np.uint8) * 255
 
 while True:
-  output = sensor.get_most_recent_data()
+  output = data_manager.return_real_time()
+  print(output)
   total_x += int(-output[0])
   total_y += int(output[1])
   background = canvas.copy()
-  print(total_x, total_y)
   cv2.circle(background, (total_x, total_y), 10, (0,0,255), -1, cv2.LINE_AA)
   cv2.imshow("frame", background)
   cv2.waitKey(1)
+
